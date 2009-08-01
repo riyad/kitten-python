ee7c632677ef45f8f5f479ec76699b3097fc7f1c
Riyad Preukschas <riyad@informatik.uni-bremen.de>
2009-08-01 03:16:58 +0200

    Tried to make the git history model a little smarter.

--- Kitten/githistorymodel.py
+++ Kitten/githistorymodel.py
@@ -5,37 +5,36 @@ from PyQt4.QtCore import *
 from git import *
 
 class GitHistoryModel(QAbstractTableModel):
+  columns = ('Summary', 'Author', 'Date', 'Id')
+  column_mapping = {'Summary': 'summary', 'Author': 'author', 'Date': 'authored_date', 'Id': 'id_abbrev'}
+
   def __init__(self, repo_path = "", branch = "master", parent = None):
     QAbstractTableModel.__init__ (self, parent)
     self.repository = Repo(repo_path)
     self.branch = branch
-    self.columnNames = ("Summary", "Author", "Date", "Id")
+    self.reset()
 
   def columnCount(self, parent = QModelIndex()):
-    return len(self.columnNames)
+    return len(self.columns)
+
+  def columnName(self, column):
+    return self.columns[column]
 
   def data(self, index, role = Qt.DisplayRole):
     if not index.isValid() or role != Qt.DisplayRole:
       return QVariant()
     else:
-      commit = self.repository.commits(start = self.branch, max_count = 1, skip = index.row())[0]
-      column = index.column()
-      if column == 0:
-        return QVariant(commit.summary)
-      elif column == 1:
-        return QVariant(commit.author)
-      elif column == 2:
-        return QVariant(commit.authored_date)
-      elif column == 3:
-        return QVariant(commit.id_abbrev)
-      else:
-        return QVariant()
+      return QVariant(getattr(self.commits[index.row()], self.column_mapping[self.columnName(index.column())]))
 
   def headerData(self, section, orientation, role = Qt.DisplayRole):
     if orientation != Qt.Horizontal or role != Qt.DisplayRole:
       return QVariant()
     else:
-      return QVariant(self.columnNames[section])
+      return QVariant(self.columnName(section))
+
+  def reset(self):
+    self.commits = self.repository.commits(start = self.branch, max_count = self.rowCount())
+    QAbstractTableModel.reset(self)
 
   def rowCount(self, parent = QModelIndex()):
     return self.repository.commit_count(self.branch)
