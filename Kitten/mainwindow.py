fc6e7fec5d771acd798a129a5947ee5b68e90347
Riyad Preukschas <riyad@informatik.uni-bremen.de>
2009-08-01 02:26:20 +0200

    Started populating history view with data.

--- Kitten/mainwindow.py
+++ Kitten/mainwindow.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 
 from ui_mainwindow import Ui_MainWindow
+from githistorymodel import GitHistoryModel
 
 from PyKDE4.kdeui import KMainWindow
 
@@ -9,3 +10,6 @@ class MainWindow(KMainWindow):
     KMainWindow.__init__(self)
     self.ui = Ui_MainWindow()
     self.ui.setupUi(self)
+
+    historyModel = GitHistoryModel(parent = self.ui.historyTableView)
+    self.ui.historyTableView.setModel(historyModel)
